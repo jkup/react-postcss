3641eb50f79641bbf623c37945f848f5db02e137
Jon Kuperman <jon.kuperman@gmail.com>
2016-09-15 22:48:18 -0700

    standard webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    entry: "./entry.js",
-    output: {
-        path: __dirname,
-        filename: "bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.css$/, loader: "style!css" }
-        ]
-    }
-};
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.css$/, loader: 'style!css' }
+    ]
+  }
+}
